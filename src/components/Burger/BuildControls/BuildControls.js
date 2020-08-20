85f4cbcdc44f2a2c812b151cf10e9db5e3e7a036
andihoerudin24 <andihoerudin24@gmail.com>
2020-08-20 13:50:46 +0700

    adding ingredinets done

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import classes from './BuildControls.css'
+import BuildControls from './BuildControls/BuildControl'
+
+const controls =  [
+    {label:'Salad',type:'salad'},
+    {label:'Bacon',type:'bacon'},
+    {label:'Cheese',type:'cheese'},
+    {label:'Meat',type:'meat'}
+]
+
+
 const buildControls = (props) => {
      return(
-         <div>
-              
+         <div className={classes.BuildControls}>
+              {controls.map(ctrl=> (
+                  <BuildControls key={ctrl.label} label={ctrl.label} 
+                  added={() =>{props.ingredientAdded(ctrl.type)}}
+                  />
+              ))}
          </div>
      )
 }
